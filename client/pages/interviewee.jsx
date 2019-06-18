b1d74168dffedfe93f14a02b09cd60cf1b5ae1df
Tarik <tarikishaq@hotmail.com>
2019-06-17 20:21:32 -0700

    Add basic interview from date

---
+++ client/pages/interviewee.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class interviewee extends Component {
+    render() {
+        return (
+            <div>
+                <h1>HI</h1>
+            </div>
+        )
+    }
+}
