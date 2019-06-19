91eaceddbdbdbc5791aad28ff4d053effc26c3a3
Ethan Tam <ethantam623@hotmail.com>
2019-06-19 15:49:03 -0700

    fixed division query

--- client/pages/edit.jsx
+++ client/pages/edit.jsx
@@ -8,7 +8,7 @@ const Container = styled.div`
   text-align: center;
 `;
 
-export default class FormPage extends Component {
+export default class EditFormPage extends Component {
   static async getInitialProps({ query }) {
     return {
       interviewId: query.interviewId
@@ -22,36 +22,10 @@ export default class FormPage extends Component {
     });
   };
 
-  createInterviewee = () => {
-    const requestBody = {
-      ...this.state,
-      status: "Unemployed",
-      age: parseInt(this.state.age, 10)
-    };
-
-    const intervieweePromise = fetch("http://localhost:8080/interviewee", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(requestBody)
-    }).then(response => {
-      return response.json();
-    });
-
-    intervieweePromise.then(interviewee => {
-      Router.push(
-        "/confirm?interviewId=" +
-          this.props.interviewId +
-          "&intervieweeId=" +
-          interviewee.id
-      );
-    });
-  };
   render() {
     return (
       <Container>
-        <h1>Please Fill Out Your Information</h1>
+        <h1>Edit Your Information</h1>
         <Form setValue={this.setValue} />
         <Button
           variant="contained"
