521688be078c783532b452440c9ab1074c81bdce
Ariel Cao <ariel207@gmail.com>
2019-06-19 20:49:48 -0700

    added even MORE availables

--- server/insert.sql
+++ server/insert.sql
@@ -26,26 +26,62 @@ INSERT INTO Provides_Amenities VALUES(2, 'accessibility', 'wheelchair ramp', '20
 INSERT INTO Provides_Amenities VALUES(3, 'supplies', 'whiteboard eraser', '2053 Main Mall, Vancouver, BC, V6T 1Z2','c');
 INSERT INTO Provides_Amenities VALUES(4, 'supplies', 'stapler', '2053 Main Mall, Vancouver, BC, V6T 1Z2','c');
 
-INSERT INTO Available VALUES(0, '2000-01-01 00:00:00', '2000-01-01 01:00:00', 0, '0', '0');
-INSERT INTO Available VALUES(5, '2019-07-02 11:00:00', '2019-07-02 12:00:00', 1, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
-INSERT INTO Available VALUES(6, '2019-07-02 14:00:00', '2019-07-02 15:00:00', 2, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
-INSERT INTO Available VALUES(7, '2019-07-02 15:00:00', '2019-07-02 16:00:00', 3, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
-INSERT INTO Available VALUES(8, '2019-07-02 16:00:00', '2019-07-02 17:00:00', 4, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
-
 INSERT INTO Available VALUES(9, '2019-05-29 16:00:00', '2019-05-29 17:00:00', 2, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
 INSERT INTO Available VALUES(10, '2019-05-27 16:00:00', '2019-05-27 17:00:00', 2, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
 INSERT INTO Available VALUES(11, '2019-05-27 18:00:00', '2019-05-27 19:00:00', 2, '5282 Fifth Street Vancouver, BC, V6A 3B9', 'j');
 INSERT INTO Available VALUES(12, '2019-05-27 19:00:00', '2019-05-27 20:00:00', 2, '800 Junior Road Vancouver, BC, V6T 2J1', 'M');
 
-INSERT INTO Available VALUES(13, '2019-07-03 11:00:00', '2019-07-03 12:00:00', 1, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
-INSERT INTO Available VALUES(14, '2019-07-03 14:00:00', '2019-07-03 15:00:00', 2, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
-INSERT INTO Available VALUES(15, '2019-07-03 15:00:00', '2019-07-03 16:00:00', 3, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
-INSERT INTO Available VALUES(16, '2019-07-03 16:00:00', '2019-07-03 17:00:00', 4, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
-
-INSERT INTO Available VALUES(17, '2019-07-04 11:00:00', '2019-07-04 12:00:00', 1, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
-INSERT INTO Available VALUES(18, '2019-07-04 14:00:00', '2019-07-04 15:00:00', 2, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
-INSERT INTO Available VALUES(19, '2019-07-04 15:00:00', '2019-07-04 16:00:00', 3, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
-INSERT INTO Available VALUES(20, '2019-07-04 16:00:00', '2019-07-04 17:00:00', 4, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(0, '2000-01-01 00:00:00', '2000-01-01 01:00:00', 0, '0', '0');
+INSERT INTO Available VALUES(5, '2019-07-02 11:00:00', '2019-07-02 12:00:00', 1, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(6, '2019-07-02 14:00:00', '2019-07-02 15:00:00', 1, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(7, '2019-07-02 15:00:00', '2019-07-02 16:00:00', 1, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(8, '2019-07-02 16:00:00', '2019-07-02 17:00:00', 1, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(13, '2019-07-02 11:00:00', '2019-07-02 12:00:00', 2, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(14, '2019-07-02 14:00:00', '2019-07-02 15:00:00', 2, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(15, '2019-07-02 15:00:00', '2019-07-02 16:00:00', 2, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(16, '2019-07-02 16:00:00', '2019-07-02 17:00:00', 2, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(17, '2019-07-02 11:00:00', '2019-07-02 12:00:00', 3, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(18, '2019-07-02 14:00:00', '2019-07-02 15:00:00', 3, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(19, '2019-07-02 15:00:00', '2019-07-02 16:00:00', 3, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(20, '2019-07-02 16:00:00', '2019-07-02 17:00:00', 3, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(21, '2019-07-02 11:00:00', '2019-07-02 12:00:00', 4, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(22, '2019-07-02 14:00:00', '2019-07-02 15:00:00', 4, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(23, '2019-07-02 15:00:00', '2019-07-02 16:00:00', 4, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(24, '2019-07-02 16:00:00', '2019-07-02 17:00:00', 4, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+--
+INSERT INTO Available VALUES(25, '2019-07-03 11:00:00', '2019-07-03 12:00:00', 1, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(26, '2019-07-03 14:00:00', '2019-07-03 15:00:00', 1, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(27, '2019-07-03 15:00:00', '2019-07-03 16:00:00', 1, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(28, '2019-07-03 16:00:00', '2019-07-03 17:00:00', 1, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(29, '2019-07-03 11:00:00', '2019-07-03 12:00:00', 2, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(30, '2019-07-03 14:00:00', '2019-07-03 15:00:00', 2, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(31, '2019-07-03 15:00:00', '2019-07-03 16:00:00', 2, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(32, '2019-07-03 16:00:00', '2019-07-03 17:00:00', 2, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(33, '2019-07-03 11:00:00', '2019-07-03 12:00:00', 3, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(34, '2019-07-03 14:00:00', '2019-07-03 15:00:00', 3, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(35, '2019-07-03 15:00:00', '2019-07-03 16:00:00', 3, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(36, '2019-07-03 16:00:00', '2019-07-03 17:00:00', 3, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(37, '2019-07-03 11:00:00', '2019-07-03 12:00:00', 4, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(38, '2019-07-03 14:00:00', '2019-07-03 15:00:00', 4, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(39, '2019-07-03 15:00:00', '2019-07-03 16:00:00', 4, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(40, '2019-07-03 16:00:00', '2019-07-03 17:00:00', 4, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+--
+INSERT INTO Available VALUES(41, '2019-07-04 11:00:00', '2019-07-04 12:00:00', 1, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(42, '2019-07-04 14:00:00', '2019-07-04 15:00:00', 1, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(43, '2019-07-04 15:00:00', '2019-07-04 16:00:00', 1, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(44, '2019-07-04 16:00:00', '2019-07-04 17:00:00', 1, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(45, '2019-07-04 11:00:00', '2019-07-04 12:00:00', 2, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(46, '2019-07-04 14:00:00', '2019-07-04 15:00:00', 2, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(47, '2019-07-04 15:00:00', '2019-07-04 16:00:00', 2, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(48, '2019-07-04 16:00:00', '2019-07-04 17:00:00', 2, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(49, '2019-07-04 11:00:00', '2019-07-04 12:00:00', 3, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(50, '2019-07-04 14:00:00', '2019-07-04 15:00:00', 3, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(51, '2019-07-04 15:00:00', '2019-07-04 16:00:00', 3, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(52, '2019-07-04 16:00:00', '2019-07-04 17:00:00', 3, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(53, '2019-07-04 11:00:00', '2019-07-04 12:00:00', 4, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(54, '2019-07-04 14:00:00', '2019-07-04 15:00:00', 4, '2053 Main Mall, Vancouver, BC, V6T 1Z2', 'a');
+INSERT INTO Available VALUES(55, '2019-07-04 15:00:00', '2019-07-04 16:00:00', 4, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
+INSERT INTO Available VALUES(56, '2019-07-04 16:00:00', '2019-07-04 17:00:00', 4, '2366 Main Mall, Vancouver, BC, V6T 1Z4', 'b');
 
 INSERT INTO Interviewee VALUES(0,'0000000000','0','0','0', 0,'0');
 INSERT INTO Interviewee VALUES(1,'604-123-1234','Felix','reviewing','600 Pine St, #403, Seattle, WA 98101',23,'felix@gmail.com');
